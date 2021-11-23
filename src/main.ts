0c90595ace0e92d10723e401ed9face126328fb8
Bonganjalo Hadebe <bonganjaloh.hadebe@gmail.com>
2021-11-23 09:01:10 +0200

    feat(main): added apollo client

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,23 @@
-import { createApp } from 'vue'
+import { createApp, provide, h } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import { DefaultApolloClient } from '@vue/apollo-composable'
+import { ApolloClient, InMemoryCache, gql } from '@apollo/client';
 
-createApp(App).use(store).use(router).mount('#app')
+const defaultClient = new ApolloClient({
+    uri: 'https://tosd6.sse.codesandbox.io/',
+    cache: new InMemoryCache()
+  })
+  
+  createApp(
+    {
+      setup () {
+        provide(DefaultApolloClient, defaultClient)
+      },
+      render() {
+        return h(App)
+      }
+    }
+  ).use(store).use(router).mount('#app')
+// createApp(App).use(store).use(router).mount('#app')
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,39 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    {{ message }}
   </div>
+    <ul>
+    <li v-for="person in people.results" :key="person.name">
+      <router-link :to="{ name: 'About', params: { name: person.name }}"><h2>{{ person.name}}</h2></router-link>
+    </li>
+  </ul>
 </template>
 
 <script lang="ts">
-import { Options, Vue } from 'vue-class-component';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
 
-@Options({
-  components: {
-    HelloWorld,
-  },
-})
-export default class Home extends Vue {}
+import { gql } from 'graphql-tag';
+import { useQuery, useResult, useMutation } from '@vue/apollo-composable'
+// import allWinesQuery from '../graphql/queries/findAll.query.gql'
+export default {
+    
+  
+   setup(){
+    const findAll = gql` query Query {
+
+    findAll {
+      results {
+        name
+        height
+        mass
+        gender
+        homeworld
+      }
+    }
+  }`
+    const { result } = useQuery(findAll)
+    const people = useResult(result, null, data => data.findAll)
+    return { people};
+    }
+
+}
 </script>
